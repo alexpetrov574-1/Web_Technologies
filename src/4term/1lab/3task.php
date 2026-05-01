30f4f783e330d841a09c17d001cc31dc30e76e73
AlexCatPRG <alexpetrov006@gmail.com>
2026-05-01 21:18:57 +0700

    2 feature(3task): added 3task

--- src/4term/1lab/3task.php
+++ src/4term/1lab/3task.php
@@ -1,2 +1,24 @@
 <?php
+echo "Таблица умножения:<br/>";
+echo "<table>";
+echo "<tr>";
+echo "<th>x</th>";
+for ($i = 1; $i <= 10; $i++)
+{
+    echo "<th>$i</th>";
+}
+echo "</tr>";
+for ($i = 1; $i <= 10; $i++)
+{
+    echo "<tr>";
+    echo "<th>$i</th>";
+    for ($j = 1; $j <= 10; $j++)
+    {
+        $result = $i * $j;
+        echo "<td>$result</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+echo "</div>";
 ?>
